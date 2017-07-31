49437bac3ce89f7dbb5f898cd6473874bec536d8
sbradno <scotty112@live.com>
2017-07-31 12:41:02 -0400

    Add

--- src/DotNetHero/Program.cs
+++ src/DotNetHero/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿// Authored by Scott B. Norton
 
-namespace dotnethero
+namespace DotNetHero
 {
+    using System;
+    using System.IO;
+    using System.Threading;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = /* I name thee... */"D o t  N e t  H e r o";
+
+            ConsoleColor[,] terrain;
+            int width;
+            int height;
+
+            using (FileStream fileStream = File.OpenRead("claycave.terrain"))
+            using (var br = new BinaryReader(fileStream))
+            {
+                string header = br.ReadString();
+                uint id = br.ReadUInt32();
+                width = br.ReadInt32();
+                height = br.ReadInt32();
+
+                terrain = new ConsoleColor[width, height];
+
+                for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                {
+                    byte access = br.ReadByte();
+                    terrain[x, y] = (ConsoleColor)br.ReadInt32();
+                }
+            }
+
+            int drawX = Console.WindowWidth;
+            int drawY = Console.WindowHeight;
+            if (width < drawX)
+                drawX -= drawX - width;
+            if (height < drawY)
+                drawY -= drawY - height;
+
+            for (int x = 0; x < drawX; x++)
+            for (int y = 0; y < drawY; y++)
+            {
+                Console.BackgroundColor = terrain[x, y];
+                Console.SetCursorPosition(x, y);
+                Console.Write(' ');
+            }
+
+            Thread.Sleep(-1);
         }
     }
 }
